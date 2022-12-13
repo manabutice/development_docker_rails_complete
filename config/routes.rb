768eb4bb7ce78826a3e5cee81c95758e564b88d0
manabutice <manabutice@gmail.com>
2022-12-14 07:05:13 +0900

    81. ユーザー認証機能で使用する ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
-# == Route Map
-#
-#     Prefix Verb   URI Pattern                Controller#Action
-#     boards GET    /boards(.:format)          boards#index
-#            POST   /boards(.:format)          boards#create
-#  new_board GET    /boards/new(.:format)      boards#new
-# edit_board GET    /boards/:id/edit(.:format) boards#edit
-#      board GET    /boards/:id(.:format)      boards#show
-#            PATCH  /boards/:id(.:format)      boards#update
-#            PUT    /boards/:id(.:format)      boards#update
-#            DELETE /boards/:id(.:format)      boards#destroy
-
 Rails.application.routes.draw do
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
