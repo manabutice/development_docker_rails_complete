9bf912e51ff36ceca220833128f23a96fe7f63d2
manabutice <manabutice@gmail.com>
2022-12-17 14:32:38 +0900

    85. ログイン、 ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)      not null
+#  password_digest :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_name  (name) UNIQUE
+#
+
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 16 },
+    format: {
+      with: /\A[a-z0-9]+\z/,
+      message: 'は小文字英数字で入力してください'
+    }
+  validates :password,
+    length: { minimum: 8 }
 end
